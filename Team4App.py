50b0d03f3493ee3f36041ab3818164659e1804f6
BaderIAlharbi <bih13@outlook.com>
2024-05-19 02:58:33 +0300

    Team4App.py

--- Team4App.py
+++ Team4App.py
@@ -19,17 +19,20 @@ def download_file(url, filename):
     return filename
 
 # URLs of the files in the GitHub repository
-pipeline_url = "https://raw.githubusercontent.com/BaderIAlharbi/Team4/main/Team4vectorizer_pipeline.pkl"
-model_url = "https://raw.githubusercontent.com/BaderIAlharbi/Team4/main/Team4model_nb.pkl"
+pipeline_url = "https://raw.githubusercontent.com/BaderIAlharbi/Team4/main/Team_4_vectorizer.pkl"
+model1_url = "https://raw.githubusercontent.com/BaderIAlharbi/Team4/main/Team_4_knn_spam_detection_model_nb.pkl"
+model2_url = "https://raw.githubusercontent.com/BaderIAlharbi/Team4/main/Team_4_nb_spam_detection_model_nb.pkl"
 image_url = "https://miro.medium.com/v2/resize:fit:1400/0*mbFBPcPUJD-53v3h.png" 
 
 try:
     # Download and load the pipeline and model
-    pipeline_path = download_file(pipeline_url, "Team4vectorizer_pipeline.pkl")
-    model_path = download_file(model_url, "Team4model_nb.pkl")
+    pipeline_path = download_file(pipeline_url, "Team_4_vectorizer.pkl")
+    model1_path = download_file(model_url, "Team_4_knn_spam_detection_model_nb.pkl")
+    model2_path = download_file(model_url, "Team_4_nb_spam_detection_model_nb.pkl")
 
     vectorizer = joblib.load(pipeline_path)
-    model = joblib.load(model_path)
+    model1 = joblib.load(model1_path)
+    model2 = joblib.load(model2_path)
 except Exception as e:
     st.error(f"Error loading model or vectorizer: {e}")
     st.stop()
@@ -79,11 +82,13 @@ if st.button("Predict"):
     if input_text:
         try:
             processed_text = vectorizer.transform([input_text])
-            prediction = model.predict(processed_text)
+            prediction1 = model1.predict(processed_text)
+            prediction2 = model2.predict(processed_text)
             #result = "Spam" if prediction[0] == 1 else "Not Spam"
             #st.write(f"Prediction: {prediction}")
             #prediction = "Spam"  # Example prediction value
-            st.markdown(f'<p class="custom-text-label">Prediction: {prediction}</p>', unsafe_allow_html=True)
+            st.markdown(f'<p class="custom-text-label">Prediction via KNN: {prediction1}</p>', unsafe_allow_html=True)
+            st.markdown(f'<p class="custom-text-label">Prediction via NB: {prediction2}</p>', unsafe_allow_html=True)
         except Exception as e:
             #st.error(f"Error during prediction: {e}")
             st.markdown(f'<p class="custom-text-label">Error during prediction: {e}</p>', unsafe_allow_html=True)
@@ -95,4 +100,4 @@ if st.button("Predict"):
 st.image(image_url, use_column_width=True)
 
 # Disclaimer
-st.markdown(f'<p class="custom-text-label">Disclaimer: This is an academic project with limited training size of 5,572 emails only!, please do not rely on this tool, cheers!</p>', unsafe_allow_html=True)
+st.markdown(f'<p class="custom-text-label">Disclaimer: This is an academic project based on a limited dataset of only 5,572 emails. Please do not rely on this tool for any critical decisions. Cheers!</p>', unsafe_allow_html=True)
