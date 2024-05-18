2bcc756aa076a37db08a5e307094c9dfb5b5bff2
BaderIAlharbi <bih13@outlook.com>
2024-05-18 19:39:26 +0300

    Team4App.py

--- Team4App.py
+++ Team4App.py
@@ -5,13 +5,12 @@ Created on Thu May  16 20:11:05 2024
 """
 
 import streamlit as st
-!pip install joblib
-import joblib
+#import joblib
 from PIL import Image
 
 # Load your trained model and vectorizer
 filepath = 'Team4model_nb.pkl'
-model = joblib.load(filepath )
+model = load(filepath )
 filepath2 = 'Team4vectorizer_pipeline.pkl'
 vectorizer = joblib.load(filepath2 )
 
